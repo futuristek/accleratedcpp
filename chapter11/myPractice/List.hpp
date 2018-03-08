a4024328baea6724ea936e8a23f4d707d6f09d1c
futuristek <jie.chen@mathworks.com>
2018-03-08 17:42:28 -0500

    blah

--- chapter11/myPractice/List.hpp
+++ chapter11/myPractice/List.hpp
@@ -4,33 +4,17 @@ template <class T> class List {
 public:
 	typedef std::size_t size_type;
 	typedef T value_type;
-
-	typedef struct {
-		T val;
-		T* pre;
-		T* next;
-	} Node;
-
-	class iterator {
-	private:
-		Node* node;
-
-	public:
-		iterator() { node = 0; }
-		T& operator* () { return node->val; }
-
-	class const_iterator {
-
-	};
+    typedef T* iterator;
+    typedef T* const const_iterator;
 
 	List() { create(); }
 	List(size_type n, const T& val = T()) { create(n, val); }
 	List(const List& l) { create(l.begin(), end()); }
 
-	iterator begin() {	return head; }
-	const_iterator begin() const { return head; }
-	iterator end() { return tail; }
-	const_iterator end() const { return tail; }
+	iterator begin() {	return &(head->val); }
+	const_iterator begin() const { return &(head->val); }
+	iterator end() { return &(tail->val); }
+	const_iterator end() const { return &(tail->val); }
 
 	void push_back(const T& val) { append(val); }
 
@@ -38,19 +22,67 @@ public:
 	iterator erase(iterator);
 	void clear();
 
-	size_type size() { return tail - head; }
+	size_type size() const { return tail - head; }
 
 	List& operator= (const List&);
 
 	~List() { uncreate(); }
 private:
-	iterator head;
-	iterator tail;
 
-	//memory management implementation
-	void create();
-	void create(size_type, const T&);
-	void create(iterator, iterator);
+	struct Node {
+		T val;
+		Node* pre;
+		Node* next;
+	};
+
+    Node* head;
+	Node* tail;
+
+	void create() { head = tail = 0; }
+	void create(size_type n, const T& val);
+	void create(const_iterator i, const_iterator j);
 	void uncreate();
 	void append(const T&);
 };
+
+template <class T>
+void List<T>::create(size_type n, const T& val) {
+    if (n < 1)
+        return;
+
+    head = new Node;
+    head->val = val;
+    head->pre = 0;
+    head->next = 0;
+
+    Node* pre = head;
+
+    for (size_type i = 1; i < n; ++i) {
+        Node* node = new Node;
+        node->val = val;
+        node->pre = pre;
+        node->next = 0;
+        pre->next = node;
+        pre = node;
+    }
+}
+
+template<class T>
+void List<T>:: create(const_iterator i, const_iterator j) {
+    create(j - i, T());
+    const_iterator ptr = i;
+    iterator this_ptr = head;
+    while (ptr != 0 ) {
+        head-> 
+        ptr = ptr->next;
+    }
+}
+
+template <class T>
+List<T>& List<T>::operator= (const List<T>& rhs) {
+    if(&rhs != this) {
+        uncreate();
+        create(rhs.begin(), rhs.end());
+    }
+    return *this;
+}
